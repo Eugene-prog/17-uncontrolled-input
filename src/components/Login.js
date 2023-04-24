221013947528e9ca935cdfd4a0d67e132d73a988
Eugene-prog <ieugene.prog@gmail.com>
2023-04-24 23:36:33 +0700

    add: handleFormSubmit function

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,18 @@
 function Login() {
+
+  function handleFormSubmit(event){
+    event.preventDefault();
+    const userData = {
+      user: event.target.username.value,
+      password: event.target.password.value
+    };
+    console.log(userData);
+  }
+
   return(
   <>
     <h1>Login Form</h1>
-    <form>
+    <form onSubmit={handleFormSubmit}>
       <label>Username:
         <input type="text" name="username"/> 
       </label>
